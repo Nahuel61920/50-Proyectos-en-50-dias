bbbc20f147dcafcf6f8d44b7fe65c2fa7de11c83
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-03-13 22:56:12 -0300

    Dia 34: Task app

---
+++ 34 Task app/script.js
@@ -0,0 +1,67 @@
+// Variables
+const formTask = document.getElementById('formTask');
+
+// EventListener
+EventListener()
+
+function EventListener() {
+  formTask.addEventListener('submit', saveTask);
+}
+
+// Funciones
+function saveTask(e) {
+  e.preventDefault();
+
+  let title = document.getElementById('title').value;
+  let description = document.getElementById('description').value;
+
+  let task = {
+    title,
+    description
+  };
+
+  if(localStorage.getItem('tasks') === null) {
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  } else {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  getTasks();
+  document.getElementById('formTask').reset();
+}
+
+function deleteTask(title) {
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  for(let i = 0; i < tasks.length; i++) {
+    if(tasks[i].title == title) {
+      tasks.splice(i, 1);
+    }
+  }
+  
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  getTasks();
+}
+
+function getTasks() {
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  let tasksView = document.getElementById('tasks');
+  tasksView.innerHTML = '';
+  for(let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+
+    tasksView.innerHTML += `<div class="card mb-3">
+        <div class="card-body">
+          <p>${title} - ${description}
+          <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">Delete</a>
+          </p>
+        </div>
+      </div>`;
+  }
+}
+
+getTasks();
--- README.md
+++ README.md
@@ -278,5 +278,13 @@ Este es un reto personal de crear un mini proyecto de html-css-javascript, por 5
 <th><a href="https://github.com/Nahuel61920/Teclado-virtual" rel="nofollow">Repository</a></th>
 </tr>
 
+<tr>
+<td align="center">34</td>
+<td>Task app</td>
+<th>09/03/22</th>
+<td><a href="https://nahuel61920.github.io/Task/">Demo</a></td>
+<th><a href="https://github.com/Nahuel61920/Task" rel="nofollow">Repository</a></th>
+</tr>
+
 </tbody>
 </table>
